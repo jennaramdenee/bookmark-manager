3cf1158ca60799368befe29ecfb10e50a0688f18
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-30 11:54:33 +0000

    Refactored test for adding tags

--- spec/features/tag_spec.rb
+++ spec/features/tag_spec.rb
@@ -1,7 +1,13 @@
 feature "tags" do
   scenario "adding tags to links on creation" do
     visit '/links/new'
-    fill_in "tags", with: "social, harambe, memes"
+    fill_in("title", :with => "BBC News")
+    fill_in("url", :with => "https://www.bbc.co.uk/news")
+    fill_in "tags", with: "news, london, tech"
+    click_button "Save Link"
+
+    expect(page).to have_content("BBC News")
+
   end
 
 end
